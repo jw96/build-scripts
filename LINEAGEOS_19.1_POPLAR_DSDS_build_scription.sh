44db13e20e34dcfce9d77855b2804920d9265ff1
jw96 <jw96@pm.me>
2022-08-12 18:08:13 +0200

    Add files via upload

---
+++ LINEAGEOS_19.1_POPLAR_DSDS_build_scription.sh
@@ -0,0 +1,16 @@
+cd /home/artek/shie/SSD/sources/los/19.1/
+source build/envsetup.sh
+mka clobber
+export USE_CCACHE=1
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 100G
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+breakfast poplar_dsds
+croot
+mka target-files-package otatools
+sign_target_files_apks -o -d ~/.android-certs     $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip     signed-target_files.zip
+ota_from_target_files -k ~/.android-certs/releasekey     --block --backup=true     signed-target_files.zip     lineage-19.1-$(date +%Y%m%d)-UNOFFICIAL-poplar_dsds.zip
+rm build.md5sum
+md5sum lineage-19.1-$(date +%Y%m%d)-UNOFFICIAL-poplar_dsds.zip > build.md5sum
+rm build.prop
+cp -f out/target/product/poplar_dsds/system/build.prop .
