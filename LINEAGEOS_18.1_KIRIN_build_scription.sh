448bc06fce00acfc730d7860cdfc479bd5154769
jw96 <jw96@pm.me>
2022-08-27 17:54:06 +0200

    Update LINEAGEOS_18.1_KIRIN_build_scription.sh
    add folders dep on build target to save build.md5sum and build.prop

--- LINEAGEOS_18.1_KIRIN_build_scription.sh
+++ LINEAGEOS_18.1_KIRIN_build_scription.sh
@@ -12,6 +12,6 @@ mka target-files-package otatools
 sign_target_files_apks -o -d ~/.android-certs     $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip     signed-target_files.zip
 ota_from_target_files -k ~/.android-certs/releasekey     --block --backup=true     signed-target_files.zip     lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-kirin.zip
 rm build.md5sum
-md5sum lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-kirin.zip > build.md5sum
+md5sum lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-kirin.zip > ./kirin/build.md5sum
 rm build.prop
-cp -f out/target/product/kirin/system/build.prop .
+cp -f out/target/product/kirin/system/build.prop ./kirin/
