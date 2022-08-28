d719e412c09289137e322cf7a856ede62285e2d0
jw96 <jw96@pm.me>
2022-08-28 16:42:12 +0200

    Update LINEAGEOS_18.1_POPLAR_DSDS_build_scription.sh

--- LINEAGEOS_18.1_POPLAR_DSDS_build_scription.sh
+++ LINEAGEOS_18.1_POPLAR_DSDS_build_scription.sh
@@ -11,7 +11,7 @@ croot
 mka target-files-package otatools
 sign_target_files_apks -o -d ~/.android-certs     $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip     signed-target_files.zip
 ota_from_target_files -k ~/.android-certs/releasekey     --block --backup=true     signed-target_files.zip     lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-poplar_dsds.zip
-rm build.md5sum
+rm ./poplar_dsds/build.md5sum
 md5sum lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-poplar_dsds.zip > ./poplar_dsds/build.md5sum
 rm ./poplar_dsds/build.prop
 cp -f out/target/product/poplar_dsds/system/build.prop ./poplar_dsds/
